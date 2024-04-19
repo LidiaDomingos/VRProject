5a3afb039e44c8d7fd915dd362dfbb6b701201bb
Lincoln <lincolninsper@gmail.com>
2024-04-19 17:10:26 -0300

    puting DetectVR Script to simulate without headset

---
+++ Assets/VRsubSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class VRsubSystem : MonoBehaviour
+
+{
+    public GameObject xrOrigin;
+    public GameObject desktopCharacter;
+
+    void Start()
+    {
+        var xrSettings = XRGeneralSettings.Instance;
+        if (xrSettings == null)
+        {
+            Debug.Log("XrGeneralSettings is null");
+            return;
+        }
+
+        var xrManager = xrSettings.Manager;
+        if (xrManager == null)
+        {
+            Debug.Log("XrManagerSettings is null");
+            return;
+        }
+
+        var xrLoader = xrManager.activeLoader;
+        if (xrLoader == null)
+        {
+            Debug.Log("XRLoader is null");
+            xrOrigin.SetActive(false);
+            desktopCharacter.SetActive(true);
+            return;
+        }
+
+        Debug.Log("XRLoader is not null");
+        xrOrigin.SetActive(true);
+        desktopCharacter.SetActive(false);
+
+    }
+}
