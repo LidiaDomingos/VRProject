b04ce855dba5edea3c41862be9a1bbec0065e404
LidiaDomingos <lidiaalves2002@outlook.com>
2024-05-17 13:28:59 -0300

    up

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerLogic : MonoBehaviour
 {
     public float health = 100f;
+    public float score = 0f;
     public bool isPlayerDead = false;
     // Start is called before the first frame update
     void Start()
